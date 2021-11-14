a4b2dab13498cd56e9f62befe2294e067dc59c19
AlexandrKostetki <70638083+AlexandrKostetki@users.noreply.github.com>
2021-11-14 18:53:09 +0200

    Add files via upload

---
+++ prob 8.p 60.py
@@ -0,0 +1,5 @@
+A={11,22,34,56,78}
+B={22,34,67,88,90}
+print('Intersectia=',A.intersection(B))
+print('Reuniunea=',A.union(B))
+print('Diferenta=',A.difference(B))
---
+++ prob 9.p60.py
@@ -0,0 +1,5 @@
+A={'E','G','F','H'}
+B={'E','M','N','H'}
+print('Intersectia=',A.intersection(B))
+print('Reuniunea=',A.union(B))
+print('Diferenta=',A.difference(B))
